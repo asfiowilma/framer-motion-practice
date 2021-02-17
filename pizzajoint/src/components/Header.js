3f8a5556b0046ce7dfb81c5fb7fa01c24237d14e
asfiowilma <asfiolithaw@gmail.com>
2021-02-17 20:40:19 +0700

    made the logo draggable :D

--- pizzajoint/src/components/Header.js
+++ pizzajoint/src/components/Header.js
@@ -21,29 +21,32 @@ const pathVar = {
 const Header = () => {
   return (
     <header>
-      <a href="/">
-        <div className="logo">
-          <motion.svg
-            variants={svgVar}
-            initial="init"
-            animate="anim"
-            className="pizza-svg"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-          >
-            <motion.path
-              variants={pathVar}
-              fill="none"
-              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-            />
-            <motion.path
-              variants={pathVar}
-              fill="none"
-              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-            />
-          </motion.svg>
-        </div>
-      </a>
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        dragElastic={1}
+      >
+        <motion.svg
+          variants={svgVar}
+          initial="init"
+          animate="anim"
+          className="pizza-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+        >
+          <motion.path
+            variants={pathVar}
+            fill="none"
+            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+          />
+          <motion.path
+            variants={pathVar}
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
+        </motion.svg>
+      </motion.div>
       <motion.div
         className="title"
         transition={{ delay: 0.2 }}
