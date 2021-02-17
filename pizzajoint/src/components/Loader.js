a381f4c3e90c433ab1df02ac4010c387912cc472
asfiowilma <asfiolithaw@gmail.com>
2021-02-17 20:32:04 +0700

    added change loader button

--- pizzajoint/src/components/Loader.js
+++ pizzajoint/src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, useCycle } from 'framer-motion';
 
 const loaderVar = {
   anim1: {
@@ -7,17 +7,25 @@ const loaderVar = {
     y: [0, -30],
     transition: {
       x: { yoyo: Infinity, duration: 0.5 },
-      y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" },
+      y: { yoyo: Infinity, duration: 0.25, ease: 'easeOut' },
+    },
+  },
+  anim2: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: { yoyo: Infinity, duration: 0.25, ease: 'easeOut' },
     },
   },
 };
 
 export default function Loader() {
+  const [animation, cycleAnimation] = useCycle('anim1', 'anim2');
+
   return (
-    <motion.div
-      className="loader"
-      variants={loaderVar}
-      animate="anim1"
-    ></motion.div>
+    <>
+      <motion.div className="loader" variants={loaderVar} animate={animation} />
+      <div onClick={() => cycleAnimation()}>Cycle Loader</div>
+    </>
   );
 }
